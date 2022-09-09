552e57a1b157c07ef92e9479f43d3c8ae6ea9f95
Tumininu <smattumi@gmai.com>
2022-09-09 15:22:02 +0100

    addede links

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const openModal2 = document.getElementById("open-modal2")
 const overlay =  document.getElementById("overlay")
 openModal.addEventListener("click", function(){
     overlay.style.display = "block"
+    overlay.style.animation = "fadeVisibility 0.5s"
 })
 openModal2.addEventListener("click", function(){
     overlay.style.display = "block"
+    overlay.style.animation = "fadeVisibility 0.5s"
 })
 
 const closeModal =  document.getElementById("close-modal")
 closeModal.addEventListener("click", function(){
-    overlay.style.display = "none"
+       overlay.style.display = "none"
+       overlay.style.animation = "openVisibility 0.5s"
+   
 })
